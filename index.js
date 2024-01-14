58226185b62bf5e24c6252c95e8298961a85eaa5
somaharshith334 <somaharshithkumar334@gmail.com>
2024-01-14 22:09:37 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 const app = require("./app");
 
 app.listen(3000, () => {
   console.log("Started express server at port 3000")
 });
-=======
 console.log("Hello GitHub!");
->>>>>>> origin/master
