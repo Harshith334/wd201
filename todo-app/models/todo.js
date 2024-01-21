d6f7439fef1a30ecaca41323c52861d4ec6eb6e9
SOMA HARSHITH KUMAR <123182324+Harshith334@users.noreply.github.com>
2024-01-21 08:41:25 +0530

    todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,4 +1,6 @@
 
+
+
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
@@ -8,14 +10,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    
     static associate(models) {
-      // define association here
+      
     }
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
+    
     static async getTodos() {
       const all = await this.findAll();
+      
       if (all.length >= 1) {
         return all;
       } else {
@@ -24,19 +29,24 @@ module.exports = (sequelize, DataTypes) => {
           dueDate: new Date().toISOString(),
           completed: false,
         });
+        
         await this.addTodo({
           title: "Buy xbox",
           dueDate: new Date().toISOString(),
           completed: false,
         });
+
+        
         const allTodos = await this.findAll();
         return allTodos;
       }
     }
+    
     markAsCompleted() {
       return this.update({ completed: true });
     }
   }
+  
   Todo.init(
     {
       title: DataTypes.STRING,
