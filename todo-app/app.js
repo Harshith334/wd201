61c509825deb0b642708276d752e1ab42e42139c
SOMA HARSHITH KUMAR <123182324+Harshith334@users.noreply.github.com>
2024-01-23 20:54:23 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.get("/", async (request, response) => {
-  const allTodos = await Todo.getTodos();
+  const allTodos = await Todo.allTodos();
   if(request.accepts("html")) {
     response.render('index', {
       allTodos
